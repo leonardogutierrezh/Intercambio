6539853a2a69c04c5ec99e1bbc8bac1ff6f38311
leonardo <leonardogutierrezh@gmail.com>
2013-12-12 20:29:08 -0430

    todos los datos en mostrar solicitud alfin!!!!!

---
+++ src/java/Clases/UniversidadPrograma.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Clases;
+
+/**
+ *
+ * @author leonardo
+ */
+public class UniversidadPrograma {
+    private String nombreuni;
+    private String nombreprograma;
+    private String mesfechaini;
+    private String aniofechaini;
+    private String mesfechafin;
+    private String aniofechafin;
+    
+    public UniversidadPrograma(){
+        super();
+    }
+
+    public String getnombreuni() {
+        return nombreuni;
+    }
+
+    public void setnombreuni(String nombreuni) {
+        this.nombreuni = nombreuni;
+    }
+    
+    public String getnombreprograma() {
+        return nombreprograma;
+    }
+
+    public void setnombreprograma(String nombreprograma) {
+        this.nombreprograma = nombreprograma;
+    }
+    
+    public String getmesfechaini() {
+        return mesfechaini;
+    }
+
+    public void setmesfechaini(String mesfechaini) {
+        this.mesfechaini = mesfechaini;
+    }
+    
+    public String getaniofechaini() {
+        return aniofechaini;
+    }
+
+    public void setaniofechaini(String aniofechaini) {
+        this.aniofechaini = aniofechaini;
+    }
+    
+    public String getmesfechafin() {
+        return mesfechafin;
+    }
+
+    public void setmesfechafin(String mesfechafin) {
+        this.mesfechafin = mesfechafin;
+    }
+    
+    public String getaniofechafin() {
+        return aniofechafin;
+    }
+
+    public void setaniofechafin(String aniofechafin) {
+        this.aniofechafin = aniofechafin;
+    }
+    
+}
