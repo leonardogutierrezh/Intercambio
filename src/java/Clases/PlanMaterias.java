8d13322ae18ca83547c92a79618b424e7c2a88cb
leonardo <leonardogutierrezh@gmail.com>
2013-12-12 20:28:56 -0430

    todos los datos en mostrar solicitud alfin!!!!!

---
+++ src/java/Clases/PlanMaterias.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Clases;
+
+/**
+ *
+ * @author leonardo
+ */
+public class PlanMaterias {
+    private String materiausb;
+    private String creditousb;
+    private String codigousb;
+    private String codigouniv;
+    private String materiauniv;
+    private String creditouniv;
+    
+    public PlanMaterias() {
+        super();
+    }
+
+    public String getmateriausb() {
+        return materiausb;
+    }
+
+    public void setmateriausb(String materiausb) {
+        this.materiausb = materiausb;
+    }
+    
+    public String getcreditousb() {
+        return creditousb;
+    }
+
+    public void setcreditousb(String creditousb) {
+        this.creditousb = creditousb;
+    }
+    
+    public String getcodigousb() {
+        return codigousb;
+    }
+
+    public void setcodigousb(String codigousb) {
+        this.codigousb = codigousb;
+    }
+    
+    public String getcodigouniv() {
+        return codigouniv;
+    }
+
+    public void setcodigouniv(String codigouniv) {
+        this.codigouniv = codigouniv;
+    }
+    
+    public String getmateriauniv() {
+        return materiauniv;
+    }
+
+    public void setmateriauniv(String materiauniv) {
+        this.materiauniv = materiauniv;
+    }
+    
+    public String getcreditouniv() {
+        return creditouniv;
+    }
+
+    public void setcreditouniv(String creditouniv) {
+        this.creditouniv = creditouniv;
+    }
+}
