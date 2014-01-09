d9acd01f60eb5b22ea692211d2be41904e509e0e
leonardo <leonardogutierrezh@gmail.com>
2014-01-08 21:54:13 -0430

    Clase universidad asignada

---
+++ src/java/Clases/UniversidadAsignada.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Clases;
+
+/**
+ *
+ * @author leonardo
+ */
+public class UniversidadAsignada {
+    private String nombreusuario;
+    private String nombre;
+    private String pais;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.nombre = pais;
+    }
+    
+    public String getNombreUsuario() {
+        return nombreusuario;
+    }
+
+    public void setNombreUsuario(String nombreusuario) {
+        this.nombreusuario = nombreusuario;
+    }
+}
